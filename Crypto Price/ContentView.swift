b5f2b2a8da65e846feb75632e43edef13edaaa1f
Anshul Kanwar <4n5hu1@protonmail.com>
2022-10-28 14:25:11 +0530

    Initial Commit

---
+++ Crypto Price/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Crypto Price
+//
+//  Created by Anshul Kanwar on 28/10/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Crypto Price/Crypto_PriceApp.swift
@@ -0,0 +1,17 @@
+//
+//  Crypto_PriceApp.swift
+//  Crypto Price
+//
+//  Created by Anshul Kanwar on 28/10/22.
+//
+
+import SwiftUI
+
+@main
+struct Crypto_PriceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
